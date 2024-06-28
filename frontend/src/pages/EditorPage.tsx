143576921b5339831cd3efab64161c3f70673624
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-28 01:47:35 -0700

    Create EditorPage.tsx

---
+++ frontend/src/pages/EditorPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CodeEditor from '../components/CodeEditor';
+
+function EditorPage() {
+  return (
+    <div>
+      <h1>Code Editor</h1>
+      <CodeEditor />
+    </div>
+  );
+}
+
+export default EditorPage;
